41c5d76645cf5c8e57cf67235b165a08ae291598
austinsantoso <santoso.austin@gmail.com>
2021-01-20 11:35:59 +0800

    initial upload attemp

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import PictureEntry, User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Picture_Entry': PictureEntry}
